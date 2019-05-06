c5dba1fe3f2558a005f7d21b0deb4f99c0b1b75d
spr15bd <bevandady@gmail.com>
2019-05-06 13:49:13 +0100

    Complete project files from glitch

---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/timestamp/:date?", function (req, res) {
+  let date = req.params.date;
+  
+  if (date==undefined) {
+    date = new Date();
+    res.json({unix: date.getTime(), utc: date.toUTCString()});
+  } else if (date.isNaN) {
+    res.json({unix: null, utc: "Invalid Date"});
+  } else {
+    date = new Date(parseInt(date));
+    res.json({unix: date.getTime(), utc: date.toUTCString()});
+  }
+  
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
